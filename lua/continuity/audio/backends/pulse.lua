e3b2977efac5c0c478ed73a1d2173eee59905e23
Quincy Jo <me@quincyjo.com>
2026-05-23 11:32:20 -0400

    Fix pulse race condition when new device is auto default. (#32)
    * Fix race condition when new sink is auto default.
    
    There was a race condition between the new and change device events and
    the server change event which cause bleeding of the new default device
    into the previous default device. This corrects that by deferring
    default switching to the server change event and always interpreting
    is_default from the last server reported default.
    
    Changes:
    - Set state.is_default based on the last server reported default rather
      than the parsed state.
    
    * Add tests for race condition.

--- lua/continuity/audio/backends/pulse.lua
+++ lua/continuity/audio/backends/pulse.lua
@@ -571,6 +571,7 @@ local function create()
 						end
 						local parsed = _parse_device_by_index(stdout, idx_str)
 						if parsed then
+							parsed.state.is_default = (parsed.meta.name == current_default_sink)
 							sink_handles.add(idx_str, parsed.state, parsed.meta)
 							if parsed.meta.name then
 								sink_name_to_idx[parsed.meta.name] = idx_str
@@ -590,9 +591,10 @@ local function create()
 							end
 							local parsed = _parse_device_by_index(stdout, idx_str)
 							if parsed then
+								parsed.state.is_default = (idx_str == current_default_sink_idx)
 								sink_handles.update(idx_str, parsed.state)
 								if parsed.state.is_default and on_sink then
-									on_sink(current_default_sink_idx, parsed.state, parsed.meta)
+									on_sink(idx_str, parsed.state, parsed.meta)
 								end
 							end
 						end)
@@ -623,6 +625,7 @@ local function create()
 						end
 						local parsed = _parse_device_by_index(stdout, idx_str)
 						if parsed then
+							parsed.state.is_default = (parsed.meta.name == current_default_source)
 							source_handles.add(idx_str, parsed.state, parsed.meta)
 							if parsed.meta.name then
 								source_name_to_idx[parsed.meta.name] = idx_str
@@ -642,9 +645,10 @@ local function create()
 							end
 							local parsed = _parse_device_by_index(stdout, idx_str)
 							if parsed then
+								parsed.state.is_default = (idx_str == current_default_source_idx)
 								source_handles.update(idx_str, parsed.state)
 								if parsed.state.is_default and on_source then
-									on_source(current_default_source_idx, parsed.state, parsed.meta)
+									on_source(idx_str, parsed.state, parsed.meta)
 								end
 							end
 						end)
