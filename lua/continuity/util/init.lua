21d89afb686587ab88af64986762cdc54a8790ab
Quincy Jo <me@quincyjo.com>
2026-06-05 22:19:06 -0400

    Add Subscriptions, WeakSubscriptions, and Scope to util/init.

--- lua/continuity/util/init.lua
+++ lua/continuity/util/init.lua
@@ -4,6 +4,9 @@ local M = {
 	app_icon = require("continuity.util.app_icon"),
 	process = require("continuity.util.process"),
 	json = require("continuity.util.json"),
+	subscriptions = require("continuity.util.subscriptions"),
+	weak_subscriptions = require("continuity.util.weak_subscriptions"),
+	scope = require("continuity.util.scope"),
 }
 
 return M
